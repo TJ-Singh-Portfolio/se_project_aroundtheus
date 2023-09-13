57b13c6822ce6cdba1b53915e05db749c367011c
TJ Singh <tsinghbusiness@hotmail.com>
2023-09-12 21:30:31 -0700

    Add basic JS code and link it in HTML

---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+let initialCards = [];
+
+let object1 = {
+  name: "Yosemite Valley",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+};
+
+let object2 = {
+  name: "Lake Louise",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+};
+
+let object3 = {
+  name: "Bald Mountains",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+};
+
+let object4 = {
+  name: "Latemar",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+};
+
+let object5 = {
+  name: "Vanoise National Park",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+};
+
+let object6 = {
+  name: "Lago di Braies",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+};
+
+initialCards.push(object1);
+
+initialCards.push(object2);
+
+initialCards.push(object3);
+
+initialCards.push(object4);
+
+initialCards.push(object5);
+
+initialCards.push(object6);
