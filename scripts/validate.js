802113128bf5bd9bda692494f6f76856e1b8536c
TJ Singh <tsinghbusiness@hotmail.com>
2023-11-17 19:59:07 -0800

    Add validate script file and corresponding CSS styles

---
+++ scripts/validate.js
@@ -0,0 +1,31 @@
+enableValidation({
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+});
+//Change these classes to match your own
+
+const showInputError (formElement, inputElement, errorMessage) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add("modal__input_type_error");
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add("modal__input-error_visible");
+};
+
+const hideInputError (formElement, inputElement) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove("modal__input_type_error");
+    errorElement.classList.remove("modal__input-error_visible");
+    errorElement.textContent = "";
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+      showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+      hideInputError(formElement, inputElement);
+    }
+  }; 
