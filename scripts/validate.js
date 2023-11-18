fd15fe95a97d2a63d9a2221f8e7ac35be86a6187
TJ Singh <tsinghbusiness@hotmail.com>
2023-11-17 21:25:54 -0800

    Fix syntax issues in validate file

--- scripts/validate.js
+++ scripts/validate.js
@@ -8,24 +8,24 @@ enableValidation({
 });
 //Change these classes to match your own
 
-const showInputError (formElement, inputElement, errorMessage) {
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.add("modal__input_type_error");
-    errorElement.textContent = errorMessage;
-    errorElement.classList.add("modal__input-error_visible");
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add("modal__input_type_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("modal__input-error_visible");
 };
 
-const hideInputError (formElement, inputElement) {
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.remove("modal__input_type_error");
-    errorElement.classList.remove("modal__input-error_visible");
-    errorElement.textContent = "";
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove("modal__input_type_error");
+  errorElement.classList.remove("modal__input-error_visible");
+  errorElement.textContent = "";
 };
 
 const checkInputValidity = (formElement, inputElement) => {
-    if (!inputElement.validity.valid) {
-      showInputError(formElement, inputElement, inputElement.validationMessage);
-    } else {
-      hideInputError(formElement, inputElement);
-    }
-  }; 
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
